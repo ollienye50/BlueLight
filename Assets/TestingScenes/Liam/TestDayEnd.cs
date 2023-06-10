4c39332e43a7eb9abb4c79598f93fa48c918b374
LiamDavie <liam.t.davie@gmail.com>
2023-06-10 19:18:48 +1000

    Built a test level and began working on the energy mechanic.

---
+++ Assets/TestingScenes/Liam/TestDayEnd.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDayEnd : MonoBehaviour
+{
+    public int test;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseUp()
+    {
+        test++;
+        EventManagerTestLiam.NextDay();
+    }
+}

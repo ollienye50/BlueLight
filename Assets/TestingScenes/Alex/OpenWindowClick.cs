a789a6b5d3e940eaaa7ffa3750cda591c095b51b
1010524Alex <1010524@student.sae.edu.au>
2023-06-14 17:00:14 +1000

    Slight modification of script

--- Assets/TestingScenes/Alex/OpenWindowClick.cs
+++ Assets/TestingScenes/Alex/OpenWindowClick.cs
@@ -14,10 +14,19 @@ public class OpenWindowClick : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Checking for pressing the mouse button
       if (Input.GetMouseButtonDown(0))
         {
+            //if over A
+                //World to screen (A)
+                    //Compare mouse position
+                // OR Screen to ray, raycast against A
+
+                //instatiate
             Debug.Log("Mouse Click");
+            //Transposing the mouse position into the world.
             Vector2 CursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //Spawning a 'window tab' object at the cursor position
             Instantiate(WindowTab, new Vector2(CursorPos.x, CursorPos.y), Quaternion.identity);
         }  
     }

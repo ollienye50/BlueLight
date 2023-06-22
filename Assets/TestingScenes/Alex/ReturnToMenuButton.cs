4b872d2b206afe45f66a9fc4f182ec502d5f658a
1010524Alex <1010524@student.sae.edu.au>
2023-06-22 16:17:22 +1000

    Added Credits Scene and Scene loader Script.

---
+++ Assets/TestingScenes/Alex/ReturnToMenuButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMenuButton : MonoBehaviour
+{
+    public void loadScene(string MainMenu)
+    {
+        SceneManager.LoadScene(MainMenu);
+    }
+    
+}

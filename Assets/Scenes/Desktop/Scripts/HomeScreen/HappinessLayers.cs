7197253f926950079874595a98b6146d61f03c15
ollienye50 <olivianye50@gmail.com>
2023-08-09 16:59:57 +1000

    fixed happiness layers

--- Assets/Scenes/Desktop/Scripts/HomeScreen/HappinessLayers.cs
+++ Assets/Scenes/Desktop/Scripts/HomeScreen/HappinessLayers.cs
@@ -7,12 +7,16 @@ public class HappinessLayers : MonoBehaviour
     public GameObject Layer1;
     public GameObject Layer2;
     public GameObject Layer3;
+    public GameObject Layer4;
+    public GameObject Layer5;
 
     void Start()
     {
         Layer1.SetActive(false);
         Layer2.SetActive(false);
         Layer3.SetActive(false);
+        Layer4.SetActive(false);
+        Layer5.SetActive(false);
     }
     
     // Update is called once per frame
@@ -23,27 +27,53 @@ public class HappinessLayers : MonoBehaviour
             Layer1.SetActive(false);
             Layer2.SetActive(false);
             Layer3.SetActive(false);
+            Layer4.SetActive(false);
+            Layer5.SetActive(false);
         }
     
-        if(EnergyManagement.instance.happiness <= 10 && EnergyManagement.instance.happiness >= 5)
+        if(EnergyManagement.instance.happiness <= 9 && EnergyManagement.instance.happiness >= 8)
         {
             Layer1.SetActive(true);
             Layer2.SetActive(false);
             Layer3.SetActive(false);
+            Layer4.SetActive(false);
+            Layer5.SetActive(false);
         }
 
-        if(EnergyManagement.instance.happiness <= 4 && EnergyManagement.instance.happiness >= 3)
+        if(EnergyManagement.instance.happiness <= 7 && EnergyManagement.instance.happiness >= 6)
         {
             Layer1.SetActive(true);
             Layer2.SetActive(true);
             Layer3.SetActive(false);
+            Layer4.SetActive(false);
+            Layer5.SetActive(false);
         }
 
-        if(EnergyManagement.instance.happiness <= 2 && EnergyManagement.instance.happiness >= 1)
+        if(EnergyManagement.instance.happiness <= 5 && EnergyManagement.instance.happiness >= 3)
         {
             Layer1.SetActive(true);
             Layer2.SetActive(true);
             Layer3.SetActive(true);
+            Layer4.SetActive(false);
+            Layer5.SetActive(false);
+        }
+
+        if (EnergyManagement.instance.happiness == 2)
+        {
+            Layer1.SetActive(true);
+            Layer2.SetActive(true);
+            Layer3.SetActive(true);
+            Layer4.SetActive(true);
+            Layer5.SetActive(false);
+        }
+
+        if (EnergyManagement.instance.happiness == 1)
+        {
+            Layer1.SetActive(true);
+            Layer2.SetActive(true);
+            Layer3.SetActive(true);
+            Layer4.SetActive(true);
+            Layer5.SetActive(true);
         }
     }
 }
